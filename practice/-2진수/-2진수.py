fed59cd0b21c70c857f6ba05cf9314fae173ea56
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2026-04-10 12:37:18 +0900

    update-2026-04-10-codingtest

--- practice/-2진수/-2진수.py
+++ practice/-2진수/-2진수.py
@@ -5,18 +5,28 @@ input = sys.stdin.readline
 # 초점을 항상 나머지가 0 또는 1이 나오도록 해야 함
 # 나눠야하는 수가 음수 + 홀수면 -1 더해서 -2로 나누기 진행 => -2로 나누면 나머지는 1이 남음. 예외로 구성
 
-
-
 n = int(input().strip())
 
 formation = []
 
-# while n != 1:
-#     if n == 0:
-#         formation.append(0)
-#         break
-#     if n % -2 == 
+while n != 1:
+    if n == 0:
+        formation.append(0)
+        break
+    if n % -2 != 0:
+        formation.append(1)
+        n -= 1
+        n //= -2
+    elif n % -2 == 0:
+        formation.append(0)
+        n //= -2
+# 시작부터 n이 1이거나 끝까지 나눠졌을 때는 n이 1임
+if n == 1:
+    formation.append(1)
+formation = formation[::-1]
 
+for i in formation:
+    print(i, end='')
 
 # # 음수 홀수인 경우
 # print((-101 +(-1)) // -2)
