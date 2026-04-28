5f5376fb8da73e4f192903c8ae4b5284049fbec0
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2026-04-28 11:59:01 +0900

    update 2026-04-28-codingtest

--- programmersPractice/해시/베스트앨범/베스트앨범.py
+++ programmersPractice/해시/베스트앨범/베스트앨범.py
@@ -1,25 +1,30 @@
 # 고쳐야 할 부분: 노래는 횟수가 겹칠 수 있어 (노래횟수, 인덱스) 형태의 튜플로 항상 가지고 다녀야 한다.
+# 남은 문제: 재생수가 똑같을 때 고유 번호가 더 낮은 걸 골라야 하는데, 그걸 고려 안하고 있음
+# => 고유 번호 자체는 오름차순으로 정렬하기
+# 걸린 부분 - 문제 이해: 장르"별"로 최대 2곡씩 뽑아내야 함. 
 
 def solution(genres, plays):
     songDict = {}
 
     for i in range(len(genres)):
         # 해당 장르에 대해 리스트 넣기(장르가 없다면)
-        if songDict.get(genres[i], 0) == 0:
-            # 배운 점: list() 함수는 해당 값을 순회가능할 때(iterable)만 사용 가능하다. 숫자는 불가.
-            # 따라서 []는 그 자체의 값을 리스트화 하기 때문에 이걸 사용하자.
-            songDict[genres[i]] = songDict.get(genres[i], [(plays[i], i)])
-        # 장르 있으면 해당 리스트에 append 함수
-        else:
-            # 배운 점: append() 함수는 None을 반환하기 때문에 =으로 받아서는 안 된다.
-            songDict.get(genres[i], 0).append((plays[i], i))
+        # 배운 점: list() 함수는 해당 값을 순회가능할 때(iterable)만 사용 가능하다. 숫자는 불가.
+        # 따라서 []는 그 자체의 값을 리스트화 한다.
+        # 배운 점: append() 함수는 None을 반환하기 때문에 =으로 받아서는 안 된다.
+        # setdefault 함수는 딕셔너리의 해당 키가 없으면 오른쪽 값을 반환. 있으면 그대로 반환.
+        songDict.setdefault(genres[i], []).append((plays[i], i))
     
-    print("songDict:", songDict)
     # sum한 딕셔너리를 만듦
     sumDict = {}
 
     # 노래 딕셔너리 내 큰 순 정렬
     for key, value in songDict.items():
+        # 키 내부의 value들만 sort - 내림차순
+        # 튜플끼리의 sort는 sort(key=lambda x: x[0])을 관용적으로 많이 쓴다
+        # 비교용 튜플을 새로 생성해서 진행한다. 첫번째 인덱스는 내림차순을 사용하기 위해 -를, 기본은 오름차순이니 아무것도 붙이지 않음.
+        # 튜플에서는 비교할 때 앞 인덱스가 1순위가 됨
+        songDict[key].sort(key=lambda x: (-x[0], x[1]))
+
         sumValue = 0
         for i in range(len(value)):
             sumValue += value[i][0]
@@ -37,55 +42,32 @@ def solution(genres, plays):
     # x에 sumDict.keys() 하나씩 넣고 sumDict[key]의 숫자값끼리 역순으로 sorted()
     sumDict = sorted(sumDict.keys(), key= lambda x: sumDict[x], reverse=True)
 
-    print("sumDict:", sumDict)
-
     max1, max2 = '', ''
     answer = []
 
-    print(songDict[0])
-
+    # 세트별로 정렬이 됐다고 가정
     # 장르 1개
     if len(sumDict) == 1:
         # 장르 1개, 노래 1개
-        if len(sumDict[0]) == 1:
-            answer.append(0)
+        if len(songDict[sumDict[0]]) == 1:
+            answer.append(songDict[sumDict[0]][0][1])
         # 장르 1개, 노래 2개 이상
         else:
-            answer.append(plays.index(sumDict[0][0]))
-            answer.append(plays.index(sumDict[0][1]))
-        return answer
+            # songDict에 있는 인덱스 그 자체를 가져옴
+            answer.append(songDict[sumDict[0]][0][1])
+            answer.append(songDict[sumDict[0]][1][1])
 
     # 장르 2개 이상
+    # 장르별로 최대 2곡씩 뽑아내야 함.
     else:
-        for i in range(2):
-            # 첫 번째로 큰 장르
-            if i == 0:
-                max1 = sumDict[0]
-            # 두 번째로 큰 장르
-            else:
-                max2 = sumDict[1]
+        for i in range(len(sumDict)):
+            # 한 곡만 있는 장르
+            if len(songDict[sumDict[i]]) == 1:
+                answer.append(songDict[sumDict[i]][0][1])
 
-        # 첫 번째로 큰 장르 + 첫 번째 노래
-        if len(songDict[max1]) == 1:
-            answer.append(plays.index(songDict[max1][0]))
-
-        # 첫 번째로 큰 장르 + 두 번째 노래    
-        elif len(songDict[max1]) >= 2:
-            answer.append(plays.index(songDict[max1][0]))
-            answer.append(plays.index(songDict[max1][1]))
-
-        # 두 번째로 큰 장르 + 첫 번째 노래    
-        if len(songDict[max2]) == 1:
-            answer.append(plays.index(songDict[max2][0]))
-        
-        # 두 번째로 큰 장르 + 두 번째 노래
-        elif len(songDict[max2]) >= 2:
-            answer.append(plays.index(songDict[max2][0]))
-    #         answer.append(plays.index(songDict[max2][1]))
-
-        return answer
-
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
-
-print(solution(genres, plays))
+            # 두 곡 이상 있는 장르
+            elif len(songDict[sumDict[i]]) >= 2:
+                answer.append(songDict[sumDict[i]][0][1])
+                answer.append(songDict[sumDict[i]][1][1])
+                
+    return answer
