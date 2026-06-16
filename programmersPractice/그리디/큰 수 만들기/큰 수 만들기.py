dde5ed409c014e9894caa14c844a3442dd5c1756
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2026-06-17 07:04:31 +0900

    docs: 큰 수 만들기.py 스택으로 재구현 중

--- programmersPractice/그리디/큰 수 만들기/큰 수 만들기.py
+++ programmersPractice/그리디/큰 수 만들기/큰 수 만들기.py
@@ -1,30 +1,19 @@
+# top과 현재 것을 계속 비교 -> 스택
+# top보다 현재 것이 크면, k가 0보다 클 때 top을 pop, k-=1
+# top보다 현재 것이 작으면 push
+
 def solution(number, k):
+
+    result = []
+
+    for i in range(len(number)):
+        while len(result) > 0 and result[-1] < number[i] and k > 0:
+            result.pop()
+            k -= 1
+        if len(result) == 0 or result[-1] >= number[i]:
+            result.append(number[i])
+            print("result = ", result, "k = ", k)
     
-    lenN = len(number)
-    maxN = 0
-    text = ""
+    return result
 
-    i = 0
-    detectRange = lenN - k
-    maxIndex = -1 # 인덱스 0부터 시작하도록 하기 위함
-    while detectRange > 0:
-
-        # 마지막 인덱스가 가장 큰 수였다면, maxIndex를 계속 고정 시킨다면 무한루프 가능성 있음
-        # 따라서 골랐다면 그 다음 인덱스부터 보도록 하게 하면 됨
-        j = maxIndex + 1
-        maxN = 0
-
-        # lenN -k -1 +i 같은 식의 경우는 직접 시작 인덱스, 끝 인덱스를 구해보면서 진행해야 함 - 이게 아님. 다른 테스트케이스에도 적용시켜봐야 함. 틀렸음
-        # k + i를 범위로 넣으면 정확하게 작동함
-        while j <= k + i:
-            if maxN < int(number[j]):
-                maxN = int(number[j])
-                maxIndex = j
-            
-            j += 1
-
-        text += str(maxN)
-        i += 1
-        detectRange -= 1
-
-    return text
+print(solution("1231234", 3))
