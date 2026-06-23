9aaab87aedd440a37eb8fa4bbd084b7a8ea7928c
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2026-06-24 06:18:00 +0900

    feat: 구명보트 그리디 알고리즘 풀이 추가 - 양쪽 포인터를 통해 움직이도록 구현

--- programmersPractice/그리디/구명보트/구명보트.py
+++ programmersPractice/그리디/구명보트/구명보트.py
@@ -1,7 +1,7 @@
 # 최대한 적게 사용 -> 그리디 알고리즘
 
 # 오름차순 정렬 후 앞에서부터 2명 짝지어보고 안되면 1명 넣고 출발
-# limit의 2명 합했을 때. [0] + [1] > limit면 거기서부터 전부 + 1. 이후부턴 전부 +1 취급
+# limit의 2명 합했을 때. [앞에서부터] + [뒤에서부터] > limit면 거기서부터 전부 + 1. 이후부턴 전부 +1 취급
 
 def solution(people, limit):
 
@@ -13,14 +13,19 @@ def solution(people, limit):
     # 스택? 큐? while문?
     # 2명씩 타는 경우(limit 이하일 때)
     i = 0
-    while i+1 < len(people) and people[i] + people[i+1] <= limit:
-        print("people[0] =", people[i], "people[1] =", people[i+1])
-        count += 1        
-        i += 2
-
-    # 초과하는 순간 거기서부터는 1명씩밖에 못 탐
-    for j in range(i, len(people)):
-        print("people[j] =", people[j])
+    j = len(people) - 1
+    while i+1 < len(people) and i < j:
+        # 두 명이 짝지어진 경우(가장 가벼운 + 무거운)
+        if people[i] + people[j] <= limit:
+            count += 1
+            j -= 1
+            i += 1
+        # 두 명이 안되는 경우(무거운 사람만)
+        else:
+            count += 1
+            j -= 1
+    
+    if i == j:
         count += 1
 
     return count
